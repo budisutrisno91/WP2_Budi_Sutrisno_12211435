e7f388f5ed3d5a5da25ec9e33fb591feed8c610e
budisutrisno91 <sutrisnobudi999@gmail.com>
2022-10-10 15:22:06 +0700

    menambahkan index.html, Latihan1.php, Matakuliah.php, Model_latihan1.php, v_about.php, v_footer.php, v_header.php, v_index.php, view-data-matakuliah.php, view-form-matakuliah.php, view-latihan1.php, Web.php

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Budi</li>
+ <li>Nama Belakang : Sutrisno</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kebon Nanas Jl. Irmas RT.08 RW.01 Kel. Cikokol Kec. Tangerang Kota Tangerang</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tangerang</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Futsal</li>
+ <li>Bersepeda</li>
+ <li>Mobile Esports</li>
+ </ul>
+</section>
